8367bb2393d6d893a51c87a42321582917359528
Sanket Kumar <sanketcharanpahadi@gmail.com>
2022-05-18 08:15:53 +0530

    script.js updated

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const jobsEl = document.querySelector(".jobs");
+const filtersEl = document.querySelector(".filters");
 let jobArray = [];
+let retake = [];
 
 // fetching data from data.json
 const getJsonData = async function () {
   const response = await fetch("./data.json");
   jobArray = await response.json();
-  console.log(jobArray);
-
+  retake = [...jobArray];
   populateDOM(jobArray);
+
+  //   console
 };
+
+getJsonData();
 
 // adding job cards to the dom
 function addToDom(array) {
@@ -36,8 +41,6 @@ function addToDom(array) {
   });
 }
 
-getJsonData();
-
 // adding new and featured tag
 function addNewFeatured(array) {
   array.forEach((item) => {
@@ -53,10 +56,83 @@ function addNewFeatured(array) {
   });
 }
 
-function addTags(array) {}
+// adding all the tags of each item
+function addTags(array) {
+  array.forEach((item) => {
+    const tagsEl = document.querySelector(`.job-tags-${item.id}`);
+    const childTag = [...item.languages, ...item.tools];
+    for (let i = 0; i < childTag.length; i++) {
+      const tagEl = ` <span class="tag">${childTag[i]}</span>`;
+      tagsEl.insertAdjacentHTML("beforeend", tagEl);
+    }
+  });
+}
 
 // calling all the functions together
 function populateDOM(array) {
   addToDom(array);
   addNewFeatured(array);
+  addTags(array);
 }
+
+function addToFiltersDOM(text) {
+  const filterEl = `<div class="filter">
+    <span>${text}</span>
+    <img src="images/icon-remove.svg" class="remove" alt="" />
+  </div>`;
+  filtersEl.insertAdjacentHTML("beforeend", filterEl);
+}
+
+// filtering the jobArray values
+function filter(text) {
+  jobArray = jobArray.filter((item) => {
+    if (
+      [...item.languages, ...item.tools, item.level, item.role].includes(text)
+    ) {
+      return item;
+    }
+  });
+  jobsEl.innerHTML = "";
+  populateDOM(jobArray);
+}
+
+function updateDOM() {
+  jobsEl.innerHTML = "";
+  const filterChildEl = filtersEl.querySelectorAll(".filter");
+  [...filterChildEl].forEach((item) => {
+    filter(item.children[0].textContent);
+  });
+}
+
+jobsEl.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.classList.contains("tag")) {
+    filtersEl.classList.add("active");
+    addToFiltersDOM(target.textContent);
+    updateDOM();
+  }
+});
+
+filtersEl.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.classList.contains("remove")) {
+    target.parentElement.remove();
+    if (filtersEl.childElementCount > 1) {
+      jobArray = [...retake];
+      const filterChildEl = filtersEl.querySelectorAll(".filter");
+      [...filterChildEl].forEach((item) => {
+        filter(item.children[0].textContent);
+      });
+    } else {
+      filtersEl.classList.remove("active");
+      jobsEl.innerHTML = "";
+      getJsonData();
+    }
+  } else if (target.classList.contains("clear")) {
+    const filterChilds = filtersEl.querySelectorAll(".filter");
+    [...filterChilds].forEach((item) => item.remove());
+    filtersEl.classList.remove("active");
+    jobsEl.innerHTML = "";
+    getJsonData();
+  }
+});
